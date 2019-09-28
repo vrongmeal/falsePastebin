18aa22e5229024db71e754f8cf5f939d7a916973
Vaibhav <vrongmeal@gmail.com>
2019-09-28 14:19:14 +0530

    Add Gasperfile.txt
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- mysql.php
+++ mysql.php
@@ -1,13 +1,15 @@
 <?php
 // Host will most probably be localhost
-$host = "localhost";
+$host = "51.158.118.84";
+// Enter the port number
+$port = "33060";
 // Enter the username
-$username = "root";
+$username = "vrongmeal";
 // Enter the password for username
 $password = "password";
 // Enter the database name
-$db_name = "falsePastebin";
-$link = new mysqli($host, $username, $password, $db_name);
+$db_name = "falsepastebin";
+$link = new mysqli($host, $username, $password, $db_name, $port);
 if($link->connect_errno){
     die("ERROR: Could not connect. " . $link->connect_error);
 }
